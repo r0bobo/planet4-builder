92468a3d60c22764a158eea956a3d9db2ee56b89
inwerpsel <pieter.vincent1988@gmail.com>
2019-11-19 16:57:58 +0100

    Let the builder fetch built assets of dev branches
    We want to keep build files outside of git as much as possible.
    A method to achieve this would be to only include build files in tagged
    release commits were they don't interfere with development.
    
    However, as each branch is tested by requiring it with composer, there
    needs to be another solution to make sure that the files for these
    branches are built as well. This is achieved here by having the builder
    fetch the branch and build the assets, pass them to the build context
    in a directory and then during build move all items in that directory to
    the respective plugin installation directories.
    
    To be able to do that the composer install command needed to be split up
    into installation without running scripts and only running the scripts,
    so that the assets can be moved to the right location in between.

--- src/rewrite_app_repos.sh
+++ src/rewrite_app_repos.sh
@@ -11,14 +11,19 @@ files=(
 branches=(
   "MASTER_THEME_BRANCH"
   "PLUGIN_BLOCKS_BRANCH"
+  "PLUGIN_GUTENBERG_BLOCKS_BRANCH"
   "PLUGIN_ENGAGINGNETWORKS_BRANCH"
+  "PLUGIN_GUTENBERG_ENGAGINGNETWORKS_BRANCH"
 )
 
 echo "rewrite_app_repos"
 
 for branch in "${branches[@]}"
 do
-  reponame=$( echo "${branch%_*}" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g')
+  reponame=planet4-$( echo "${branch%_*}" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g')
+  composer_dev_prefix="dev-"
+  # temp solution, would be better if this script got branch without dev- before it
+  git_branch=${!branch#"$composer_dev_prefix"}
 
   if [ -n "${!branch}" ] ; then
 
@@ -29,9 +34,17 @@ do
       if [ -e "$f" ]
       then
         echo " - $f"
-        sed -i "s|\"greenpeace\\/planet4-${reponame}\" : \".*\",|\"greenpeace\\/planet4-${reponame}\" : \"${!branch}\",|g" "${f}"
+        sed -i "s|\"greenpeace\\/${reponame}\" : \".*\",|\"greenpeace\\/${reponame}\" : \"${!branch}\",|g" "${f}"
       fi
     done
+
+    #dev branches do not include the built assets, only master does
+    git --recurse-submodules --single-branch --branch ${git_branch} https://greenpeace.com/${reponame}
+    time npm ci --prefix ${reponame} ${reponame}
+    time npm run-script --prefix ${reponame} build
+    mkdir ${HOME}/source/built-dev-assets/${reponame}
+    cp ${reponame}/assets/build/* ${HOME}/source/built-dev-assets/${reponame}
+    rm -rf ${reponame}
 
     echo "And now, delete any cached version of this package"
     rm -rf "${HOME}/source/cache/files/greenpeace/planet4-master-theme"
